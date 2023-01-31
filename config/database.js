5847b8bde0d882a06c50bf0824b3b74a0cdd40b6
Dudy83 <guillaume.zehren@live.fr>
2023-01-31 20:14:02 +0100

    yo

--- config/database.js
+++ config/database.js
@@ -25,7 +25,7 @@ module.exports = ({ env }) => ({
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "LMS"),
-      user: env("DATABASE_USERNAME", "postgres"),
+      user: env("DATABASE_USERNAME", "manne"),
       password: env("DATABASE_PASSWORD", "root"),
 
     },
--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   app: {
     keys: env.array('APP_KEYS'),
   },
   
-  url: env('PUBLIC_URL', 'https://admin.metacodeshare.com'),
+  // url: env('PUBLIC_URL', 'https://admin.metaweboffice.com'),
 });
